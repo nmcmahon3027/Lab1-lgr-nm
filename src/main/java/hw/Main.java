826501b3c35207d7bc55615f86f62cacf944bdad
nmcmahon <nmcmahon@luc.edu>
2018-06-06 11:51:38 -0500

    Lab1 Part3: To test initial solution from part 2, you could perhaps do assertEquals("fizzBuzz", FizzBuzz.eval(17)); Also maybe you could use an assumeTrue expression to show/express the contraint(s)

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -4,11 +4,42 @@ import java.util.Arrays;
 
 public class Main {
 
+  public String[] fizzBuzzArray(final int n) {
+    if (n <= 0) {
+      return null;
+    }
+    String result[] = new String[n];
+    for (int i = 1; i <= n; i++) {
+      if (i % 3 == 0 && i % 5 == 0) {
+        result[i - 1] = "fizz buzz";
+      } else if (i % 3 == 0) {
+        result[i - 1] = "fizz";
+      } else if (i % 5 == 0) {
+        result[i - 1] = "buzz";
+      } else {
+        result[i - 1] = String.valueOf(i);
+      }
+    }
+    return result;
+  }
+
   public static void main(final String[] args) {
-    System.out.println("args = " + Arrays.asList(args));
-    final HelloWorld instance = new HelloWorld();
-    System.out.println(instance.getMessage());
-    System.out.println(instance.getYear());
-    System.out.println("bye for now");
+    Main main = new Main();
+    int max = 0;
+    if (args.length > 0) {
+      try {
+        max = Integer.parseInt(args[0]);
+      } catch (NumberFormatException e) {
+        System.err.println("The argument entered must be an integar ");
+        System.exit(1);
+      }
+
+    }
+    String result[] = main.fizzBuzzArray(max);
+    if (result != null)
+      for (int i = 0; i < result.length; i++) {
+        System.out.println(result[i]);
+      }
   }
 }
+
